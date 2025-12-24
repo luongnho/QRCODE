371c48fa7467e8ae09aa4230195f39d7213fa597
luongnho <31965357+luongnho@users.noreply.github.com>
2025-12-24 22:59:34 +0700

    vercel update

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   define: {
-    // Inject environment variable from build machine (Netlify) into client code
+    // Vite sẽ thay thế process.env.API_KEY bằng giá trị từ môi trường build của Vercel
     'process.env.API_KEY': JSON.stringify(process.env.API_KEY || '')
   },
   build: {
